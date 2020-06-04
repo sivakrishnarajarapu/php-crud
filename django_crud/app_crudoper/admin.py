1777ea57d0fd8020289040cdf7ca09db914e3294
siva96723 <rsivakrishna@jgu.edu.in>
2020-06-05 00:52:08 +0530

    crud operations in php

--- django_crud/app_crudoper/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from app_crudoper.models import Student
-
-admin.site.register(Student)
--- django_crud/app_crudoper/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppCrudoperConfig(AppConfig):
-    name = 'app_crudoper'
